cc770cec6ac06c3261563c16c586268c16531fe8
mjorrico <jordanenrico214@gmail.com>
2022-10-23 16:44:53 +0200

    pawn moves generator and ChessError custom exception

--- src/ChessEngine.py
+++ src/ChessEngine.py
@@ -1,11 +1,13 @@
 """
-This class is responsible to store current game state and deciding all valid moves.
+This class is responsible to store current game state and listing all valid moves.
 """
-from numpy import array
+from numpy import array, concatenate, reshape
 from move import Move
 
-black_pieces = ["p", "b", "n", "r", "q", "k"]
-white_pieces = ["P", "B", "N", "R", "Q", "K"]
+from chesserror import ChessError
+
+black_pieces = ["p", "n", "b", "r", "q", "k"]
+white_pieces = ["P", "N", "B", "R", "Q", "K"]
 
 
 class Chessboard:
@@ -26,9 +28,24 @@ class Chessboard:
         self.white_to_move = True
         self.move_log = []
 
+    @property
+    def foe_pieces(self):
+        return black_pieces if self.white_to_move else white_pieces
+
+    @property
+    def friend_pieces(self):
+        return white_pieces if self.white_to_move else black_pieces
+
     def make_move(self, m: Move):
-        self.board[m.start_row, m.start_col] = m.piece_moved
-        self.board[m.end_row, m.end_col] = "."
+        if m not in self.get_valid_moves():
+            raise ChessError(f"Invalid move {m}")
+
+        self.board[m.end_row, m.end_col] = (
+            m.promote_to
+            if m.promote_to
+            else self.board[m.start_row, m.start_col]
+        )
+        self.board[m.start_row, m.start_col] = "."
         self.white_to_move = not self.white_to_move
         self.move_log.append(m)
 
@@ -36,53 +53,167 @@ class Chessboard:
         if len(self.move_log) != 0:
             m: Move = self.move_log.pop()
             self.board[m.start_row, m.start_col] = m.piece_moved
-            self.board[m.end_row, m.end_col] = m.piece_captured
             self.white_to_move = not self.white_to_move
+            if m.enpassant_capt_sq is None:
+                self.board[m.end_row, m.end_col] = m.piece_captured
+            else:
+                enpassant_row = m.enpassant_capt_sq[0]
+                enpassant_col = m.enpassant_capt_sq[1]
+                self.board[m.end_row, m.end_col] = "."
+                self.board[enpassant_row, enpassant_col] = 3
 
-    def get_valid_moves(self):  # removes invalid possible moves
+    def get_valid_moves(self):  # removes invalid moves from possible moves
         return self.get_possible_moves()
 
     def get_possible_moves(self):  # moves returned may be invalid
-        foe_pieces = black_pieces if self.white_to_move else white_pieces
+        all_possible_moves = []
+
         for i in range(8):
             for j in range(8):
                 current_piece = self.board[i, j]
-                if current_piece == "." or current_piece in foe_pieces:
+                if current_piece == "." or current_piece in self.foe_pieces:
                     continue
                 else:
                     if current_piece.lower() == "p":
-                        pass
+                        all_possible_moves += self.get_pawn_moves(i, j)
                     elif current_piece.lower() == "b":
-                        pass
+                        all_possible_moves += self.get_bishop_moves(i, j)
                     elif current_piece.lower() == "n":
-                        pass
+                        all_possible_moves += self.get_knight_moves(i, j)
                     elif current_piece.lower() == "r":
-                        pass
+                        all_possible_moves += self.get_rook_moves(i, j)
                     elif current_piece.lower() == "q":
-                        pass
+                        all_possible_moves += self.get_queen_moves(i, j)
                     elif current_piece.lower() == "k":
-                        pass
+                        all_possible_moves += self.get_king_moves(i, j)
                     else:
-                        raise ValueError("[CHESS_ERROR] The piece is unknown")
+                        raise ChessError("The piece is unknown")
+
+        return all_possible_moves
 
     def get_pawn_moves(self, r, c):
-        pass
+        pawn_moves = []
+
+        if self.white_to_move:  # moving white pawn
+
+            # just pawn move
+            if self.board[r - 1, c] == ".":
+                if r == 1:  # promotion
+                    for p in self.friend_pieces[1:-1]:
+                        pawn_moves.append(Move([1, c], [0, c], self.board, p))
+                else:
+                    pawn_moves.append(Move([r, c], [r - 1, c], self.board))
+                    if r == 6 and self.board[4, c] == ".":  # double move
+                        pawn_moves.append(Move([6, c], [4, c], self.board))
+
+            # pawn move with regular capture
+            capt_moves = [
+                None if c == col else [r - 1, c + i]
+                for col, i in zip([0, 7], [-1, 1])
+            ]
+            for m in capt_moves:
+                if m is not None and self.board[m[0], m[1]] in self.foe_pieces:
+                    if r == 1:  # capture and promote
+                        for p in self.friend_pieces[1:-1]:
+                            pawn_moves.append(Move([r, c], m, self.board, p))
+                    else:  # just capture
+                        pawn_moves.append(Move([r, c], m, self.board))
+
+            # pawn move with enpassant capture
+            if r == 3:
+                last_move: Move = self.move_log[-1]
+                if (
+                    last_move.piece_moved == self.foe_pieces[0]  # enemy's pawn
+                    and last_move.start_row == 1
+                    and last_move.end_row == 3  # double move
+                    and abs(last_move.start_col - c) == 1  # column diff is 1
+                ):
+                    end_col = last_move.start_col  # or last_move.end_col
+                    pawn_moves.append(
+                        Move(
+                            [r, c],
+                            [2, end_col],
+                            self.board,
+                            None,
+                            [3, end_col],
+                        )
+                    )
+
+        else:  # moving black pawn
+
+            # just pawn move
+            if self.board[r + 1, c] == ".":
+                if r == 6:  # promotion
+                    for p in self.friend_pieces[1:-1]:
+                        pawn_moves.append(Move([6, c], [7, c], self.board, p))
+                else:
+                    pawn_moves.append(Move([r, c], [r + 1, c], self.board))
+                    if r == 1 and self.board[3, c] == ".":  # double move
+                        pawn_moves.append(Move([1, c], [3, c], self.board))
+
+            # pawn move with regular capture
+            capt_moves = [
+                None if c == col else [r + 1, c + i]
+                for col, i in zip([0, 7], [-1, 1])
+            ]
+            for m in capt_moves:
+                if m is not None and self.board[m[0], m[1]] in self.foe_pieces:
+                    if r == 6:  # capture and promote
+                        for p in self.friend_pieces[1:-1]:
+                            pawn_moves.append(Move([r, c], m, self.board, p))
+                    else:  # just capture
+                        pawn_moves.append(Move([r, c], m, self.board))
+
+            # pawn move with enpassant capture
+            if r == 4:
+                last_move: Move = self.move_log[-1]
+                if (
+                    last_move.piece_moved == self.foe_pieces[0]  # enemy's pawn
+                    and last_move.start_row == 6
+                    and last_move.end_row == 4  # double move
+                    and abs(last_move.start_col - c) == 1  # column diff is 1
+                ):
+                    end_col = last_move.start_col  # or last_move.end_col
+                    pawn_moves.append(
+                        Move(
+                            [r, c],
+                            [5, end_col],
+                            self.board,
+                            None,
+                            [4, end_col],
+                        )
+                    )
+
+        return pawn_moves
 
     def get_bishop_moves(self, r, c):
-        pass
+        bishop_moves = []
+
+        return bishop_moves
 
     def get_knight_moves(self, r, c):
-        pass
+        knight_moves = []
+
+        return knight_moves
 
     def get_rook_moves(self, r, c):
-        pass
+        rook_moves = []
+
+        return rook_moves
 
     def get_queen_moves(self, r, c):
-        pass
+        queen_moves = []
+
+        return queen_moves
 
     def get_king_moves(self, r, c):
-        pass
+        king_moves = []
+
+        return king_moves
 
     def __str__(self):
+        indexing_ui = array([["x", "0", "1", "2", "3", "4", "5", "6", "7"]])
+        board_ui = concatenate((indexing_ui[:, 1:], self.board), axis=0)
+        board_ui = concatenate((indexing_ui.T, board_ui), axis=1)
         info_1 = "\nWhite to move" if self.white_to_move else "\nBlack to move"
-        return str(self.board) + info_1
+        return str(board_ui) + info_1
--- src/move.py
+++ src/move.py
@@ -1,3 +1,5 @@
+from functools import cached_property
+from typing import List
 import numpy as np
 
 black_pieces = ["p", "b", "n", "r", "q", "k"]
@@ -10,21 +12,50 @@ class Move:
     row_to_rank = {i: str(8 - i) for i in range(8)}
     rank_to_row = {str(8 - i): i for i in range(8)}
 
-    def __init__(self, start, end, board: np.ndarray):
+    def __init__(
+        self,
+        start,
+        end,
+        board: np.ndarray,
+        promote_to=None,
+        enpassant_capt_sq: List = None,
+    ):
         self.start_row = start[0]
         self.start_col = start[1]
         self.end_row = end[0]
         self.end_col = end[1]
         self.piece_moved = board[self.start_row, self.start_col]
-        self.piece_captured = board[self.end_row, self.end_col]
-
-    @property
-    def chess_notation(self):
-        prefix = self.piece_moved + self.get_file_rank(
-            self.start_row, self.start_col
+        self.promote_to = promote_to
+        self.enpassant_capt_sq = enpassant_capt_sq
+        self.piece_captured = (
+            board[enpassant_capt_sq[0], enpassant_capt_sq[1]]
+            if enpassant_capt_sq
+            else board[self.end_row, self.end_col]
         )
-        prefix += "x" if self.piece_captured != "." else ""
-        return prefix + self.get_file_rank(self.end_row, self.end_col)
+
+    @cached_property
+    def chess_notation(self):
+        notation = "" if self.piece_moved in ["p", "P"] else self.piece_moved
+        notation += self.get_file_rank(self.start_row, self.start_col)
+        notation += "x" if self.piece_captured != "." else ""
+        notation += self.get_file_rank(self.end_row, self.end_col)
+        notation += f"={self.promote_to}" if self.promote_to else ""
+        return notation
 
     def get_file_rank(self, r, c):
         return self.col_to_file[c] + self.row_to_rank[r]
+
+    def __repr__(self):
+        return self.chess_notation
+
+    def __eq__(self, other: object) -> bool:
+        return (
+            self.start_row == other.start_row
+            and self.start_col == other.start_col
+            and self.end_row == other.end_row
+            and self.end_col == other.end_col
+            and self.piece_moved == other.piece_moved
+            and self.promote_to == other.promote_to
+            and self.enpassant_capt_sq == other.enpassant_capt_sq
+            and self.piece_captured == other.piece_captured
+        )
